77fa9b5ec6edba9d395453148d05b41eccd94f8c
notroot <notroot@email.server>
2018-01-22 18:25:10 -0800

    commit

--- src/package1/Board.java
+++ src/package1/Board.java
@@ -19,7 +19,8 @@ public class Board extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        
+    	super.paintComponent(g);
 
         drawDonut(g);
     }
@@ -28,8 +29,7 @@ public class Board extends JPanel {
 
         Graphics2D g2d = (Graphics2D) g;
 
-        RenderingHints rh
-                = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
@@ -44,8 +44,7 @@ public class Board extends JPanel {
         g2d.setColor(Color.gray);
 
         for (double deg = 0; deg < 360; deg += 5) {
-            AffineTransform at
-                    = AffineTransform.getTranslateInstance(w/2, h/2);
+            AffineTransform at = AffineTransform.getTranslateInstance(w/2, h/2);
             at.rotate(Math.toRadians(deg));
             g2d.draw(at.createTransformedShape(e));
         }
