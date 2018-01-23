01a51084a597672400c7166693afc4661a0cc7fe
notroot <notroot@email.server>
2018-01-22 17:44:26 -0800

    commit

---
+++ src/Documentation.java
@@ -0,0 +1,8 @@
+public class Documentation
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Documentation");
+		// http://zetcode.com/tutorials/javagamestutorial/
+	}
+}
---
+++ src/package1/Test_package1.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Test_package1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Test: class Test_package1");
+		// http://zetcode.com/tutorials/javagamestutorial/
+	}
+}
