26a283f96d2a762d7b30b3a3bfceb2843f5d1219
notroot <notroot@email.server>
2018-01-22 18:23:46 -0800

    commit

--- src/package1/Board.java
+++ src/package1/Board.java
@@ -29,11 +29,9 @@ public class Board extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
 
         RenderingHints rh
-                = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
-                        RenderingHints.VALUE_ANTIALIAS_ON);
+                = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        rh.put(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
+        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
         g2d.setRenderingHints(rh);
 
